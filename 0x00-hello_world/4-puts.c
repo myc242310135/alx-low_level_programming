c97caf99904fb2aeb5d86c14896680238383894e
myc242310135 <myc242310135@gmail.com>
2023-06-15 17:58:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+put("/" programming is like building a multilingual puzzle");
+Return (0);}
