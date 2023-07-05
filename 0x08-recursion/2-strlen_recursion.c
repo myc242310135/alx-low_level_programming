76da3b6ce67934b70c28afda2f510d406020258a
myc242310135 <myc242310135@gmail.com>
2023-07-05 04:27:39 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
+	int longit = 0;
 
 	if (*s)
 	{
 		longit++;
-		length += _strlen_recursion(s + 1);
+		longit += _strlen_recursion(s + 1);
 	}
-	return (length);
+	return (longit);
 }
